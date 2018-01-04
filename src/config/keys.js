14f592a64dce4adbda5f04907b1781533ac54d51
DESKTOP-96V209Hridwan <ridwanfansuri@gmail.com>
2018-01-04 21:16:56 +0700

    no message

---
+++ src/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  firebaseApiKey: 'AIzaSyCGKuqcLIBM8_Cc4okaUg_mzYANcgpcLUE',
+  firebaseAuthDomain: 'sportspot-dev.firebaseapp.com',
+  firebaseDatabaseURL: 'https://sportspot-dev.firebaseio.com',
+  firebaseProjectId: 'sportspot-dev',
+  firebaseStorageBucket: 'sportspot-dev.appspot.com',
+  firebaseMessagingSenderId: '964068237678',
+};
