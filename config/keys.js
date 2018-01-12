dfd8cf5e723eabb19f71ec071fa93b0d4b3fd6b8
DESKTOP-96V209Hridwan <ridwanfansuri@gmail.com>
2018-01-12 16:16:22 +0700

    change package

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-// keys.js
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
+  module.exports.require('./prod');
 } else {
-  module.exports = require('./dev');
+  module.exports.require('./dev');
 }
